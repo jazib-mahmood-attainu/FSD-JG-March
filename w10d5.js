6a81d64c6a8c10bb5b9adb29fb1592367b142769
jazib-sudo <jazib.prof@gmail.com>
2022-05-06 22:49:52 +0530

    W10d5

--- w10d5.js
+++ w10d5.js
@@ -81,3 +81,26 @@ var findPeakElement = function(nums) {
         }
     }
 };//peak element ->https://leetcode.com/problems/find-peak-element/
+
+//samecode binary search
+var findPeakElement = function(nums){
+
+    let l = 0
+    let r = nums.length-1
+
+
+    while(l<=r){
+        mid = Math.floor((l+r)/2)
+        if((mid-1<0||nums[mid]>nums[mid-1]) && ((mid+1>=nums.length)||nums[mid]>nums[mid+1])){
+            return mid
+        }
+        if(mid-1>= 0 && nums[mid-1]>=nums[mid]){
+            r = mid-1
+        }
+        else if(mid+1 < nums.length && nums[mid+1]>=nums[mid]){
+            l = mid+1
+        }
+    }
+    return mid
+    
+};
