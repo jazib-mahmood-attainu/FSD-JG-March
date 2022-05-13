99b5b4233ebed695a11b66f84a932b929b5cae0c
jazib-sudo <jazib.prof@gmail.com>
2022-05-14 00:02:27 +0530

    W11d5

--- W11d5.js
+++ W11d5.js
@@ -14,41 +14,41 @@ function merge(A,l,m,r){ //O(n) time
         L[i] = A[l+i]
     }
     R = []
-    for(let i = 0;i<n2;i++){
-        R[i] = A[m+1+i]
+    for(let j = 0;j<n2;j++){
+        R[j] = A[m+1+j]
     }
     //I have L and R and blank C
-    C = []
     i = 0
     j = 0
     k = l
     while(i<n1 && j<n2){
         if(L[i]<R[j]){
-            C[k]=L[i]
+            A[k]=L[i]
             i++
         }
         else{
-            C[k] = R[j]
+            A[k] = R[j]
             j++
         }
        k++ 
     } //untill one of the arrays get over
 
     while(j<n2){
-        C[k]=R[j]
+        A[k]=R[j]
         j++
         k++
     }// when L is over but R has some numbers remaining
     while(i<n1){
-        C[k]=L[i]
+        A[k]=L[i]
         i++
         k++
     }// when R is over but L has some numbers remaining
-    return C
+    // return A
+    console.log(A,L,R,l,m,r);
 }
 
 function mergeSort(A,l,r){
-    m = Math.floor((l+r)/2)
+    let m = Math.floor((l+r)/2)
     if(l<r){
         mergeSort(A,l,m)
         mergeSort(A,m+1,r)
@@ -58,9 +58,10 @@ function mergeSort(A,l,r){
 }
 
 
-A = [1,3,5,7,2,4,6,8]
+A = [8,7,6,5,4,3,2,1]
 l = 0
 r = A.length-1
+console.log(A);
+mergeSort(A,l,r)
+console.log(A);
 
-res = mergeSort(A,l,r)
-console.log(res);
--- master_class.py
+++ master_class.py
@@ -1,1 +1,1 @@
-print("Hi Varsha")
+print("Hi student")
