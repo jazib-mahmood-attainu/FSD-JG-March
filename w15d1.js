e2422fa931d293f75d3bc8154ee2874b49fcf502
jazib-sudo <jazib.prof@gmail.com>
2022-06-07 23:02:59 +0530

    heapsort

--- w15d1.js
+++ w15d1.js
@@ -1,6 +1,27 @@
-function max_heapify(arr,n,i){
+function heap_sort(arr){
+    arr = build_max_heap(arr);
+    for(let  i = n-1 ;i>=0;i--)
+    {
+        temp = arr[0];
+        arr[0] = arr[i]; 
+        arr[i] = temp;
+        max_heapify(arr,i,0) //n-1/n-2/n-3/n-4
+    }
+    return arr
+}
+
+
+function build_max_heap(arr){ //O(n)
+    n = arr.length
+    for(let i =Math.floor(n/2)-1;i>=0;i--){
+        max_heapify(arr,n,i)
+    }
+    return arr
+}
+
+function max_heapify(arr,n,i){//O(height)
     let largest = i;
-    console.log(largest)
+    // console.log(largest)
     let l = 2*i+1;
     let r = 2*i+2;
     if(l<n && arr[l]>arr[largest]){
@@ -19,6 +40,37 @@ function max_heapify(arr,n,i){
 }
 
 arr = [9,33,57,2,7,1,10]
-let n = arr.length
-max_heapify(arr,n,0)
-console.log(arr)
+arr = build_max_heap(arr)
+console.log(arr);
+
+arr = heap_sort(arr)
+console.log(arr);
+// let n = arr.length
+// max_heapify(arr,n,0)
+// console.log(arr)
+
+
+///Balanced
+// var height = function(node){
+//     if(node == null){
+//         return 0
+//     }
+//     return Math.max(height(node.left),height(node.right))+1
+// }
+// var isBalanced = function(root) {
+//     if(root == null){
+//         return true
+//     }
+    
+//     let lh = height(root.left) //3
+//     let rh = height(root.right) //3
+    
+//     let l = isBalanced(root.left) //l = false
+//     let r = isBalanced(root.right) //r = false
+    
+//     if(Math.abs(lh-rh)<=1 && l==true && r==true){
+//         return true
+//     }
+    
+//     return false
+// };
